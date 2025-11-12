f2e70f8a51489fcafae1ca474029f35defdac77b
akajammythakkar <akajammythakkar@gmail.com>
2025-11-13 00:39:42 +0530

    added root agent and yaml

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,10 +1,1 @@
-"""
-Agents module for Tech Hiring Framework
-"""
-
-from .job_description_processor import JobDescriptionProcessorAgent
-from .resume_evaluator import ResumeEvaluatorAgent
-from .github_analyzer import GitHubAnalyzerAgent
-from .final_verdict import FinalVerdictAgent
-
-__all__ = ['JobDescriptionProcessorAgent', 'ResumeEvaluatorAgent', 'GitHubAnalyzerAgent', 'FinalVerdictAgent']
+from . import agent
